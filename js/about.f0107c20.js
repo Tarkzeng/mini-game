36776fe10a961d3abdc9380d7ac2b1a2c9b34964
Tarkzeng <295515452@qq.com>
2022-01-22 11:08:13 +0800

    Add files via upload

---
+++ js/about.f0107c20.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),c={class:"about"},o=Object(e["i"])("h1",null,"This is an about page",-1),s=[o];function u(n,t){return Object(e["z"])(),Object(e["h"])("div",c,s)}var i=a("d959"),b=a.n(i);const r={},d=b()(r,[["render",u]]);t["default"]=d}}]);
+//# sourceMappingURL=about.f0107c20.js.map
